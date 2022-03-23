68b7cfffcf6c26a4cc99e812d1e4c480d878c5c8
abdelRahim00 <abdelrhimsaad138@gmail.com>
2022-03-23 06:44:05 +0200

    adding all files

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const socket = require("socket.io");
+const app = express();
+const server = require("http").Server(app);
+let usersConnected = 0;
+let printName = "";
+const port = 3000;
+
+//App setup
+app.use(express.static("public"));
+server.listen(port, () => {
+   console.log(`listening on localhost:${port}`);
+});
+
+const checkNumberOfUsers = () => {
+  console.log(`Users connected:${usersConnected} `);
+};
+
+//Socket setup
+const io = socket(server);
+
+//Liten for possible connection with client
+io.on("connection", socket => {
+  usersConnected++;
+  checkNumberOfUsers();
+
+  //username received
+  socket.on("username", data => {
+    io.sockets.emit("username", data);
+  });
+
+  //Message events from the client
+  socket.on("newMessage", data => {
+    io.sockets.emit("newMessage", data);
+  });
+
+  socket.on("typing", data => {
+    socket.broadcast.emit("typing", data);
+  });
+
+  socket.on("stopTyping", data => {
+    socket.broadcast.emit("stopTyping", data);
+  });
+});
